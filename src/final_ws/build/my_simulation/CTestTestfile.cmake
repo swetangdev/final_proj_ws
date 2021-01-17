745dae550db3fc0ef7696982d9d7b4d7633305c8
iamdevgeek <iamdevgeek@gmail.com>
2021-01-17 03:35:57 -0800

    extra ws removed

--- src/final_ws/build/my_simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/swetang/final_project_ws/src/my_simulation
-# Build directory: /home/swetang/final_project_ws/build/my_simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/final_ws/build/turtlebot3_slam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/swetang/final_project_ws/src/turtlebot3_slam
-# Build directory: /home/swetang/final_project_ws/build/turtlebot3_slam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
