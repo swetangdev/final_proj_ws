eca7551da1a64cc4abb81236f3c095b2eb0785fd
iamdevgeek <iamdevgeek@gmail.com>
2021-01-16 18:07:00 -0800

    code added

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/swetang/final_project_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/swetang/final_project_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/swetang/final_project_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/swetang/final_project_ws/src:$ROS_PACKAGE_PATH"
